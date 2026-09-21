48a4548ec0d55b4b9393b12f937fc0622a3c0f70
AlejandroD999 <alejandro.gonzalez.dev8@gmail.com>
2026-09-21 18:01:34 -0500

    Implemented find_ratio for ranking

--- src/rank.py
+++ src/rank.py
@@ -1,8 +1,23 @@
-
+from difflib import SequenceMatcher 
 
 class Ranker:
     def __init__(self, data):
         self.data = data
+    
+    def find_ratio(self, title, keyword):
+        # TODO Exception handling? 
 
-    def accuracy_sort(self):
-        pass 
+        title = title.strip().lower()
+        keyword = keyword.strip().lower()
+        
+        return SequenceMatcher(None, title, keyword).ratio()
+
+    def sort(self, keyword):
+        # score based on proximity to keyword
+        # if 'matt' is keyword 'matt' has a score of 1, meanwhile mater is some other nomber of matchin letters
+        pass
+        
+if __name__ == "__main__":
+    r = Ranker("")
+
+    print(r.find_score("Some guy named Matt", "Matt"))
